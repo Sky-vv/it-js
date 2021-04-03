0a331322003fab970682c7d8ad1fc48c58f5bd3a
sky-vv <1841019@mail.ru>
2021-04-03 22:48:07 +0300

    hw1 ready 2

--- hw1/script.js
+++ hw1/script.js
@@ -50,58 +50,64 @@ let width = 10;
 let s = height * width;
 console.log(s, 'см2 площадь прямоугольника', 'Задание 4');
 
-let s1 =  ` ${height * width} см2 `;
-console.log(s1, 'см2 площадь прямоугольника - второй вариант выводы.', 'Задание 4');
+let s1 = ` ${height * width} см2 `;
+console.log(
+  s1,
+  'см2 площадь прямоугольника - второй вариант выводы.',
+  'Задание 4'
+);
 
 // Задание 5
 
 let heightC = 10;
 let dC = 4;
-let v = Math.round(Math.PI * Math.pow((dC / 2),2) * heightC);
+let v = Math.round(Math.PI * Math.pow(dC / 2, 2) * heightC);
 console.log(v, 'Объем цилиндра', 'Задание 5');
 
 // Задание 6
 
 let n = 3;
 let m = 4;
-let k = Math.sqrt((Math.pow(n,2)) + (Math.pow(m,2)));
+let k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
 console.log(k, 'Гипотенуза k', 'Задание 6');
 
-let k2 = Math.sqrt(n**2 + m**2);
+let k2 = Math.sqrt(n ** 2 + m ** 2);
 console.log(k, 'Гипотенуза k - второй вариант вывода', 'Задание 6');
 
-// Задание *1 
+// Задание *1
 
 function fib(n) {
-    let a = 1;
-    let b = 1;
-    for (let i = 3; i <= n; i++) {
-      let c = a + b;
-      a = b;
-      b = c;
-    }
-    return b;
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
   }
-console.log(fib(11), 'двенадцатый элемент последовательности. Задание *1'); 
+  return b;
+}
+console.log(fib(11), 'двенадцатый элемент последовательности. Задание *1');
 
-// Задание *2 
+// Задание *2
 
 function ipoteka(price, percent, years) {
-    let per = percent / 100 / 12;
-    let monthN = years * 12;
-    let r = price * ((per * Math.pow(1 + per, monthN)) / ( Math.pow(1 + per, monthN) - 1));
-    return r.toFixed(2);
+  let per = percent / 100 / 12;
+  let monthN = years * 12;
+  let r =
+    price *
+    ((per * Math.pow(1 + per, monthN)) / (Math.pow(1 + per, monthN) - 1));
+  return r.toFixed(2);
 }
-  
-let price   = 2000000,
-    percent = 10,
-    years   = 5;
-let amount = ipoteka(price, percent, years) * (years * 12);
-let perepl = (ipoteka(price, percent, years) * (years * 12)) - price;
 
-console.log("Общая сумма кредита: " + price + " руб");
-console.log("Ставка: " + percent + " %");
-console.log("Срок кредита: " + years + " лет");
-console.log("Ежемесячный платеж: " + ipoteka( price, percent, years) + " руб");
-console.log("Общая сумма выплат: " + amount.toFixed(2) + " руб");
-console.log("Переплата по кредиту:" + perepl.toFixed(2) + " руб");
+let price = 2000000,
+  percent = 10,
+  years = 5;
+let amount = ipoteka(price, percent, years) * (years * 12);
+let perepl = ipoteka(price, percent, years) * (years * 12) - price;
+
+console.log('Общая сумма кредита: ' + price + ' руб');
+console.log('Ставка: ' + percent + ' %');
+console.log('Срок кредита: ' + years + ' лет');
+console.log('Ежемесячный платеж: ' + ipoteka(price, percent, years) + ' руб');
+console.log('Общая сумма выплат: ' + amount.toFixed(2) + ' руб');
+console.log('Переплата по кредиту:' + perepl.toFixed(2) + ' руб');
